e22758a02c2d91c401b5e1ddd157ab4a42567ecd
cppswengr <cppswengr@eVantageGroup.com>
2018-10-30 13:06:33 -0500

    workingon perceptron; tests pass; top of page 27

--- ch2/perceptron_tests.py
+++ ch2/perceptron_tests.py
@@ -11,3 +11,40 @@ def no_training_data_supplied_test():
 
     # or this way, pick the one you like
     nt.assert_is_none(result, 'Should have no result with no training data.')
+
+
+def train_an_OR_function_test():
+
+    the_perceptron = Perceptron()
+
+    the_perceptron.train([
+                            [1, 1],
+                            [0, 1],
+                            [1, 0],
+                            [0, 0],
+                          ],
+                          [1,1,1,0])
+
+    nt.assert_equal(the_perceptron.predict([1, 1]), 1)
+    nt.assert_equal(the_perceptron.predict([1, 0]), 1)
+    nt.assert_equal(the_perceptron.predict([0, 1]), 1)
+    nt.assert_equal(the_perceptron.predict([0, 0]), 0)
+
+
+def detect_values_greater_than_five_test():
+    the_perceptron = Perceptron()
+    the_perceptron.train([
+                            [ 5, -1],
+                            [ 2, -1],
+                            [ 0, -1],
+                            [-2, -1],
+                          ],
+                          [1, 0, 0, 0])
+
+    nt.assert_equal(the_perceptron.predict([8, -1]),  1)
+    nt.assert_equal(the_perceptron.predict([5, -1]),  1)
+    nt.assert_equal(the_perceptron.predict([2, -1]),  0)
+    nt.assert_equal(the_perceptron.predict([0, -1]),  0)
+    nt.assert_equal(the_perceptron.predict([-2, -1]), 0)
+
+
