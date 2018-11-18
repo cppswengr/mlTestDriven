00ad77664ed9edf11da154b4288822d28b259a61
cppswengr <cppswengr@eVantageGroup.com>
2018-11-18 02:10:54 -0600

    Finished ch2; successfully tested perceptron

--- ch2/perceptron.py
+++ ch2/perceptron.py
@@ -2,13 +2,15 @@ class Perceptron:
 
     def train(self, inputs, labels):
 
-        self._weights = [0.431, 0.02, 0.2]
         dummied_inputs = [x + [-1] for x in inputs]
+        self._weights = [0.2] * len(dummied_inputs[0])
 
-        for _ in range(4):
+        for _ in range(6000):
 
             for input, label in zip(dummied_inputs, labels):
-                label_delta = (label - self.predict(input))
+                prediction = self.predict(input)
+                label_delta = (label - prediction)
+                # label_delta = (label - self.predict(input))
 
                 for index, x in enumerate(input):
                     self._weights[index] += .1 * x * label_delta
@@ -19,7 +21,9 @@ class Perceptron:
             return None
 
         input = input + [-1]
-        return int(0 < self._weights[0] * input[0] + self._weights[1] * input[1])
+        return int(0 < sum([x[0] * x[1] for x in zip(self._weights, input)]))
+
+        # return int(0 < self._weights[0] * input[0] + self._weights[1] * input[1])
 
         # weight_input_pairings = zip(self._weights, input)
         # weight_input_products = [x[0]*x[1] for x in weight_input_pairings]
--- ch2/perceptron_tests.py
+++ ch2/perceptron_tests.py
@@ -1,4 +1,5 @@
 import nose.tools as nt
+import numpy as np
 
 from perceptron import Perceptron
 
@@ -46,5 +47,64 @@ def detect_values_greater_than_five_test():
     nt.assert_equal(the_perceptron.predict([2]),  0)
     nt.assert_equal(the_perceptron.predict([0]),  0)
     nt.assert_equal(the_perceptron.predict([-2]), 0)
+
+
+def detect_a_complicated_example_test():
+
+    # Create random variables
+    n = 100
+
+    inputs = list(map(list, zip(np.random.uniform(0, 100, n),
+                                np.random.uniform(0, 100, n),
+                                np.random.uniform(0, 100, n)
+                               )
+                      )
+                  )
+
+    labels = [int(x[0] +x[1] + x[2] < 150) for x in inputs]
+    the_perceptron = Perceptron()
+    the_perceptron.train(inputs, labels)
+
+    n = 1000
+    test_inputs = list(map(list, zip(np.random.uniform(0, 100, n),
+                                     np.random.uniform(0, 100, n),
+                                     np.random.uniform(0, 100, n)
+                                    )
+                           )
+                       )
+
+    test_labels = [int(x[0] + x[1] + x[2] < 150) for x in test_inputs]
+
+    lti = len(test_inputs)
+    ltl = len(test_labels)
+
+    # Create separate test cases
+    false_positives = 0
+    true_positives = 0
+    false_negatives = 0
+    true_negatives = 0
+    for input, label in zip(test_inputs, test_labels):
+        prediction = the_perceptron.predict(input)
+        if prediction == 1:
+            if label == 1:
+                true_positives += 1
+            else:
+                false_positives += 1
+        else:
+            if label == 0:
+                true_negatives += 1
+            else:
+                false_negatives += 1
+
+    # Make sure we generated as much data as we'd expect
+    nt.assert_equal(false_positives + true_positives + true_negatives +
+                        false_negatives, n)
+
+    correctly_classified = true_positives + true_negatives
+    print('correctly classified:', correctly_classified/n * 100, "%")
+    assert correctly_classified > n * .9, \
+        "Perceptron should be much better than random. {0} \
+            correct".format(correctly_classified)
+
 
 
